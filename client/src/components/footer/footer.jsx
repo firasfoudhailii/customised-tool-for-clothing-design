8202f1abef8010f4b15f98ff149078f32216305b
wu-t-ang <oussema.touaibi1@esprit.tn>
2021-03-20 18:10:03 +0100

    footer

--- client/src/components/footer/footer.jsx
+++ client/src/components/footer/footer.jsx
@@ -1,11 +1,93 @@
 import React from 'react';
 import './footer.styles.scss';
+import { Button } from '../button/Button';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     const year = new Date().getFullYear();
     return (
-        <div className='footer'>
-            {year} © NoName
+        <div className='footer-container'>
+            <section className='footer-subscription'>
+            <p className="footer-subscription-heading">
+            The latest digital interconnection platform for the fashion industry. 
+            </p>
+            <p className="footer-subscription-text">
+                 Join Us.
+                 </p>
+                 <div className="input-areas">
+                     <form>
+                         <input type="email" name="email" placeholder="Your email" className="footer-input" />
+                     <Button buttonStyle='btn--outline'>Subscribe</Button>
+                     </form>
+                 </div>
+            </section>
+            <div className="footer-links">
+                <div className="footer-link-wrapper">
+                    <div className="footer-link-items">
+                        <h2>About Us</h2>
+                        <Link to='/'>How it works</Link>
+                    </div>
+          
+                    <div className="footer-link-items">
+                        <h2>Contact Us</h2>
+                        <Link to='/'>Contact</Link>
+                        <Link to='/'>Support</Link>
+                        <Link to='/'>Sponsorships</Link>
+                        <Link to='/'>partnerships</Link>
+                    </div>
+             
+            
+                    <div className="footer-link-items">
+                        <h2>Events</h2>
+                        <Link to='/'></Link>
+                    </div>
+                    <div className="footer-link-items">
+                        <h2>Social Media</h2>
+                        <Link to='/'>Instagram</Link>
+                        <Link to='/'>Facebook</Link>
+                        <Link to='/'>Youtube</Link>
+                        <Link to='/'>Twitter</Link>
+                    </div>
+                    </div>
+            </div>
+            <section className="social-media">
+                <div className="social-media-wrap">
+                    <div className="footer-logo">
+                        <Link className="social-logo">
+                            {/* Logo will be here*/}
+                        </Link>
+                    </div>
+                    <small className="website-rights">NoName © {year} All rights reserved. </small>
+                <div className="social-icons">
+                    
+                            <Link 
+                            className="social-icon-link facebook" 
+                            to='/' target='_blank' 
+                            aria-label='Facebook'>
+                            <i className="fab fa-facebook-f"></i>
+                    </Link>
+
+                    <Link 
+                            className="social-icon-link instagram" 
+                            to='/' target='_blank' 
+                            aria-label='Instagram'>
+                             <i className="fab fa-instagram"></i>
+                    </Link>
+                    <Link 
+                            className="social-icon-link twitter" 
+                            to='/' target='_blank' 
+                            aria-label='Twitter'>
+                             <i className="fab fa-twitter"></i>
+                    </Link>
+                    <Link 
+                            className="social-icon-link youtube" 
+                            to='/' target='_blank' 
+                            aria-label='Youtube'>
+                             <i className="fab fa-youtube"></i>
+                    </Link>
+                </div>
+                </div>
+            </section>
         </div>
     );
 }
