1cee050fdba47a39da48516af59919ad85c9e8d0
wu-t-ang <oussema.touaibi1@esprit.tn>
2021-03-31 02:20:07 +0200

    Lazy Loading

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,33 +1,56 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import Login from './components/login/login';
-import Signup from './components/signup/signup';
-import Homepage from './pages/front/home-page';
-import Contactpage from './pages/front/contact-page';
-import About from './pages/front/about-page';
-import NotFound from './pages/front/notfound-page';
-import AllPrototypes from './pages/front/all-prototype-page';
+import React, { Suspense } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+const Login = React.lazy(() => import('./components/login/login'));
+const Signup = React.lazy(() => import ('./components/signup/signup'));
+const Homepage = React.lazy(() => import ('./pages/front/home-page'));
+const Contactpage = React.lazy(() => import ('./pages/front/contact-page'));
+const About = React.lazy(() => import ('./pages/front/about-page'));
+const NotFound = React.lazy(() => import ('./pages/front/notfound-page'));
+const AllPrototypes = React.lazy(() => import ('./pages/front/all-prototype-page'));
 
 
 // Components 
-import Home from './components/Back/MainView/Home/Home';
-import Profile from './components/Back/MainView/Profile/Profile';
-import Prototypes from './components/Back/MainView/Prototypes/Prototypes';
+const Home = React.lazy(() => import ('./components/Back/MainView/Home/Home'));
+const Profile = React.lazy(() => import ('./components/Back/MainView/Profile/Profile'));
+const Prototypes = React.lazy(() => import ('./components/Back/MainView/Prototypes/Prototypes'));
 
 const Routes = () => {
     return (
+        <BrowserRouter basename="/">
+        <Suspense fallback={<p>...Loading page please wait</p>}>
         <Switch>
-            <Route exact path='/' component={Home} />
-            <Route path='/profileb' component={Profile} />
-            <Route path='/prototypesb' component={Prototypes} />
-            <Route path='/signup' component={Signup} />
-            <Route path='/login' component={Login} />
-            <Route path='/Home' component={Homepage} />
-            <Route path='/contactus' component={Contactpage}/>
-            <Route path='/About' component={About} />
-            <Route path='/Prototypes' component={AllPrototypes} />
-            <Route path='*' component={NotFound}/>
+            
+            <Route path='/profileb' 
+            render={(props) => <Profile {...props} />} >
+            </Route>
+            <Route path='/prototypesb' 
+           render={(props) => <Prototypesb {...props} />} >
+           </Route>
+            <Route path='/signup' 
+            render={(props) => <Signup {...props} />} >
+            </Route>            
+            <Route path='/login'
+            render={(props) => <Login {...props} />} >
+            </Route>
+            <Route exact path='/Home' 
+            render={(props) => <Homepage {...props} />} >
+            </Route>
+            <Route path='/contactus' 
+            render={(props) => <Contactpage {...props} />} >
+            </Route>
+            <Route path='/About' 
+            render={(props) => <About {...props} />} >
+            </Route>
+            <Route path='/Prototypes'
+            render={(props) => <AllPrototypes {...props} />} >
+            </Route>
+            <Route path='*' 
+            render={(props) => <NotFound {...props} />} >
+            </Route>
         </Switch>
+        </Suspense>
+        </BrowserRouter>
     )
 }
 
